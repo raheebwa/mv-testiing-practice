3d7819ed437d7182b5218b7491ff242622787c19
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-26 22:54:50 +0300

    Setup project directories

--- run.sh
+++ run.sh
@@ -25,108 +25,23 @@ touch webpack.config.js
 git add .
 git commit -m "Added webpack & config file"
 
-# 6. Add css support
-npm install --save-dev style-loader css-loader
-touch src/style.css
 
-echo "
-.hello {
-  color: red;
-}
-" >> src/style.css
-
-git add .
-git commit -m "Add css loader to webpack"
-
-# 7. Add Images support
-npm install --save-dev file-loader
-git add .
-git commit -m "Add images loader to webpack"
-
-# 8. Add data support
-npm install --save-dev csv-loader xml-loader
-
-git add .
-git commit -m "Add data loader to webpack"
-
-# 10. Manage index.html with webpack
-
-touch src/print.js
-
-echo "
-export default function printMe() {
-  console.log('I get called from print.js!');
-}
-" >> src/print.js
-
-# 10.1 
-
-npm install --save-dev html-webpack-plugin
-git add .
-git commit -m "Setting up HtmlWebpackPlugin"
-
-# 10.2 
-
-npm install --save-dev clean-webpack-plugin
-git add .
-git commit -m "Cleaning up the dist folder"
-
-# 11. Add config to webpack for asset mamgement 
 echo "
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
+    main: './src/index.js',
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Setup Webpack App',
-    }),
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ],
-    },
-    {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: [
-        'file-loader',
-      ],
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        'file-loader',
-      ],
-    },
-    {
-      test: /\.(csv|tsv)$/,
-      use: [
-        'csv-loader',
-      ],
-    },
-    {
-      test: /\.xml$/,
-      use: [
-        'xml-loader',
-      ],
-    },
-  ],
-},
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
 };" >>webpack.config.js
 
 git add .
