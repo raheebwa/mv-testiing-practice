5d4473e9d213f394df89336dfcd6143a0bc7d15c
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-28 21:23:49 +0300

    fix: bug on shift greater than 1

--- caesarCipher/ceasar.js
+++ caesarCipher/ceasar.js
@@ -11,14 +11,14 @@ const ceasar = {
     str.forEach((val, ind) => {
       if (str[ind].match(/[a-z]/)) {
         if (str[ind] === 'z') {
-          str[ind] = refLower[ceasar.getIndex(refLower, 'a')];
+          str[ind] = refLower[shift - 1 + ceasar.getIndex(refLower, 'a')];
         } else {
           val = val.toLowerCase();
           str[ind] = refLower[shift + ceasar.getIndex(refLower, val)];
         }
       } else if (str[ind].match(/[A-Z]/)) {
         if (str[ind] === 'Z') {
-          str[ind] = refUpper[ceasar.getIndex(refUpper, 'A')];
+          str[ind] = refUpper[shift - 1 + ceasar.getIndex(refUpper, 'A')];
         } else {
           val = val.toUpperCase();
           str[ind] = refUpper[shift + ceasar.getIndex(refUpper, val)];
