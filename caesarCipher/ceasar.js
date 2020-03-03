e786667b9ae4d869b9ef15972c253c68c3350989
Santiago Rodríguez <santo1996.29@gmail.com>
2020-03-02 20:38:46 -0400

    just polished it
    is weird how you can't change the string while iterating, anyway, I created a tracking string and solved it

--- caesarCipher/ceasar.js
+++ caesarCipher/ceasar.js
@@ -1,36 +1,50 @@
-const refLower = 'abcdefghijklmnopqrstuvwxyz';
-const refUpper = refLower.toUpperCase();
+const alphabetLowcase = 'abcdefghijklmnopqrstuvwxyz';
+const alphabetUpcase = alphabetLowcase.toUpperCase();
 
-const ceasar = {
-  getIndex(arr, value) {
-    return arr.split('').indexOf(value);
+const caesar = {
+  getIndex(alphabet, value) {
+    return alphabet.indexOf(value); // you always pass a string with the alphabet
   },
-  cipher(str, shift = 1) {
-    const ciphered = [];
-    str = str.split('');
-    str.forEach((val, ind) => {
-      if (str[ind].match(/[a-z]/)) {
-        if (ceasar.getIndex(refLower, str[ind]) >= 26 - shift) {
-          str[ind] = refLower[ind % (26 - shift)];
+  // the shift can also be to the left according to https://en.wikipedia.org/wiki/Caesar_cipher
+  cipher(str, shift = 0) {
+    let ciphered = '';
+
+    str.split('').forEach((value, index) => {
+
+      if (value.match(/[a-z]/)) { // you don't need to use str[ind]
+        const indexAtAlphabet = caesar.getIndex(alphabetLowcase, value);
+
+        if (indexAtAlphabet + shift >= 26) {
+          ciphered += alphabetLowcase[(indexAtAlphabet + shift) % 26];
+        } else if (indexAtAlphabet + shift < 0) {
+          ciphered += alphabetLowcase[26 + indexAtAlphabet + shift];
+          // for example, 'a' is at index 0, 0 - 1 = -1
+          // and 26 - 1 = 25 and that gives 25
         } else {
-          val = val.toLowerCase();
-          str[ind] = refLower[shift + ceasar.getIndex(refLower, val)];
+          // just add the shift and swap
+          ciphered += alphabetLowcase[shift + indexAtAlphabet];
         }
-      } else if (str[ind].match(/[A-Z]/)) {
-        if (ceasar.getIndex(refUpper, str[ind]) >= 26 - shift) {
-          str[ind] = refUpper[ind % (26 - shift)];
+      } else if (value.match(/[A-Z]/)) {
+        const indexAtAlphabet = caesar.getIndex(alphabetUpcase, value);
+
+        if (indexAtAlphabet + shift >= 26) {
+          ciphered += alphabetUpcase[(indexAtAlphabet + shift) % 26];
+        } else if (indexAtAlphabet + shift < 0) {
+          ciphered += alphabetUpcase[26 + indexAtAlphabet + shift];
         } else {
-          val = val.toUpperCase();
-          str[ind] = refUpper[shift + ceasar.getIndex(refUpper, val)];
+          ciphered += alphabetUpcase[shift + indexAtAlphabet];
         }
       }
-      ciphered.push(str[ind]);
+
     });
 
-    return ciphered.join('');
+    // you only return the string because you are changing the
+    // character itself in each iteration, and do not need to
+    // create an unnecessary tracking collection
+    return ciphered;
   },
 };
 
-export default ceasar;
+export default caesar;
 
-// console.log(ceasar.cipher('AazZ'))
+// console.log(caesar.cipher('AazZ', 1));
